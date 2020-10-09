7145c221df0beaf5763c09379959a5930adb8b13
Altynbek <altiwbek@gmail.com>
2020-10-09 18:13:25 +0600

    Fix roles update checkboxes

---
+++ MobilePhones/wwwroot/js/custom.js
@@ -0,0 +1,14 @@
+﻿
+var items = $('.js-user-role-items');
+
+$(items).on('click', function () {
+    var checkBox = $(this).find("input.form-check-input");
+
+    if (checkBox.length > 0) {
+        if ($(checkBox).is(":checked")) {
+            $(checkBox).prop("checked", false);
+        } else {
+            $(checkBox).prop("checked", true);
+        }
+    }
+});
